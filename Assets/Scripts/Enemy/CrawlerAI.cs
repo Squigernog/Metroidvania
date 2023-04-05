54151b45375bcffd84936a8e541e6b8b5d8f7856
Alex Chheng <Alex.Chheng@student.csulb.edu>
2023-04-05 15:14:10 -0700

    Enemy Movement Added

--- Assets/Scripts/Enemy/CrawlerAI.cs
+++ Assets/Scripts/Enemy/CrawlerAI.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// https://www.noveltech.dev/simple-patrolling-monster-unity/
 public class CrawlerAI : EnemyAI
 {
     public float movement = 5;
+    private bool isGoingRight = true;
 
     private void OnEnable()
     {
         rb = GetComponent<Rigidbody2D>();
+        isGoingRight=true;
     }
 
     private void FixedUpdate()
     {
-        transform.position = new Vector2(0, movement);
+        Vector3 directionTranslation = (isGoingRight) ? transform.right : -transform.right;
+        directionTranslation *= Time.deltaTime * movement;
+
+        transform.Translate(directionTranslation);
     }
 }

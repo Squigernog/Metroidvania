17b35ab17b6658850b4ced889a73826cfcda9151
Alex Chheng <Alex.Chheng@student.csulb.edu>
2023-04-12 03:16:16 -0700

    Update CrawlerAI.cs

--- Assets/Scripts/Enemy/CrawlerAI.cs
+++ Assets/Scripts/Enemy/CrawlerAI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // https://www.noveltech.dev/simple-patrolling-monster-unity/
+// https://www.youtube.com/watch?v=RuvfOl8HhhM&t=71s&ab_channel=MoreBBlakeyyy
 public class CrawlerAI : EnemyAI
 {
     public float movement = 5;
@@ -46,5 +47,29 @@ public class CrawlerAI : EnemyAI
 
             }
         }
+    }
+
+    private void CheckForHole()
+    {
+        Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
+
+        // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
+
+        // if we hit something, check its tag and act accordingly
+        if (hit.collider != null)
+        {
+            if (hit.transform.tag == "Wall")
+            {
+                bIsGoingRight = !bIsGoingRight;
+                _mSpriteRenderer.flipX = bIsGoingRight;
+
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        
     }
 }

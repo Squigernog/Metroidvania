176f83d0539be565b0bc8d8353f894ecb22ea7e7
Alex Chheng <Alex.Chheng@student.csulb.edu>
2023-04-16 01:05:57 -0700

    EnemyPatrol and Sprite
    Following the tutorial

--- Assets/Scripts/Enemy/CrawlerAI.cs
+++ Assets/Scripts/Enemy/CrawlerAI.cs
@@ -6,69 +6,7 @@ using UnityEngine;
 // https://www.youtube.com/watch?v=RuvfOl8HhhM&t=71s&ab_channel=MoreBBlakeyyy
 public class CrawlerAI : EnemyAI
 {
-    public float movement = 5;
-    public float mRaycastingDistance = 1f;
-    private bool bIsGoingRight = true;
-    private SpriteRenderer _mSpriteRenderer;
-
-    private void OnEnable()
-    {
-        _mSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        _mSpriteRenderer.flipX = bIsGoingRight;
-
-        rb = GetComponent<Rigidbody2D>();
-        bIsGoingRight=true;
-    }
-
-    private void FixedUpdate()
-    {
-        Vector3 directionTranslation = (bIsGoingRight) ? transform.right : -transform.right;
-        directionTranslation *= Time.deltaTime * movement;
-
-        transform.Translate(directionTranslation);
-
-        CheckForWalls();
-    }
-
-    private void CheckForWalls()
-    {
-        Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
-
-        // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
-
-        // if we hit something, check its tag and act accordingly
-        if (hit.collider != null)
-        {
-            if (hit.transform.tag == "Wall")
-            {
-                bIsGoingRight = !bIsGoingRight;
-                _mSpriteRenderer.flipX = bIsGoingRight;
-
-            }
-        }
-    }
-
-    private void CheckForHole()
-    {
-        Vector3 raycastDirection = (bIsGoingRight) ? Vector3.right : Vector3.left;
-
-        // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
-        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
-
-        // if we hit something, check its tag and act accordingly
-        if (hit.collider != null)
-        {
-            if (hit.transform.tag == "Wall")
-            {
-                bIsGoingRight = !bIsGoingRight;
-                _mSpriteRenderer.flipX = bIsGoingRight;
-
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
+    void Start()
     {
         
     }
